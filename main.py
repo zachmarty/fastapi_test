dc65b9d8534eaac795b552eb4735a96b9fc9a19a
Pavel <pashapasha-2013@mail.ru>
2024-09-16 10:48:20 +0300

    postgres

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from models import create_tables, drop_tables
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await drop_tables()
+    print('clear')
+    await create_tables()
+    print('created')
+    yield
+
+app = FastAPI(lifespan=lifespan)
--- models.py
+++ models.py
@@ -1,11 +1,16 @@
 import datetime
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Text, DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, Integer, String, Text, DateTime
 from sqlalchemy.sql import func
+from dotenv import load_dotenv
+from pathlib import Path
 
+load_dotenv(Path(__file__).resolve().parent / '.env')
+print(Path(__file__).resolve().parent / '.env')
 engine = create_async_engine(
-    "sql+aiosqlite:///proj.db"
+    os.getenv("DATABASE_URL"), echo = True
 )
 
 new_session = async_sessionmaker(engine, expire_on_commit = False)
@@ -18,10 +23,25 @@ class Notes(Base):
 
     id:Mapped[int] = mapped_column(primary_key = True)
     name:Mapped[str] = mapped_column(String(50))
-    content:Mapped[str] = mapped_column(Text(1000))
+    content:Mapped[str] = mapped_column(Text)
     creation_date:Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default = func.now())
     last_update:Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default = func.now())
+
+    tags = relationship("Tags", back_populates = 'note', cascade = 'all, delete')
+
+class Tags(Base):
+    __tablename__ = 'tags'
+
+    id:Mapped[int] = mapped_column(primary_key = True)
+    name:Mapped[str] = mapped_column(String(20))
+    note_id:Mapped[int] = mapped_column(Integer, ForeignKey('notes.id'))
+
+    note = relationship("Notes", back_populates = 'tags')
 
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+async def drop_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
